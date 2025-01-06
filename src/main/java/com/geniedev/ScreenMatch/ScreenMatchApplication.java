c0929dcc36b0ceba26f6fa49bcd6f8c52b60497d
Emiliano R <ev.rdz22@outlook.com>
2025-01-05 21:24:44 -0600

    Creacion del repositorio

---
+++ src/main/java/com/geniedev/ScreenMatch/ScreenMatchApplication.java
@@ -0,0 +1,18 @@
+package com.geniedev.ScreenMatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenMatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Proyecto Spring");
+	}
+}
